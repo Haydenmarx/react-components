5cc17b2e2769d1fa7a5f361537ca438c6790acbc
Hayden Marx <haydenmarx@gmail.com>
2017-12-13 10:25:45 -0800

    Create grocery list component and two list item compenents

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// TODO
+var GroceryList = () => (
+  <ul>
+    <Eggs />
+    <Chicken />
+  </ul>
+);
+
+var Chicken = () => (
+  <li>Chicken</li>
+);
+
+var Eggs = () => (
+  <li>Eggs</li>
+)
+
+// Use these two new components inside your GroceryList component instead of the hardcoded <li>s
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
