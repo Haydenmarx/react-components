fec940f635b581de9beead9492fac43969888a17
Hayden Marx <haydenmarx@gmail.com>
2017-12-13 10:38:43 -0800

    Refactored the list to take in parameters

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-var GroceryList = () => (
+var App = () => (
+  <div>
+    <GroceryList item={['milk', 'eggs', 'cheese', 'butter']}/>
+  </div>
+)
+
+var GroceryList = (list) => (
   <ul>
-    <Eggs />
-    <Chicken />
+    {/* list.forEach(item =>{
+      return <li> {item} </li>
+    }) */}
+    <li> {list.item[0]} </li>
+    <li> {list.item[1]} </li>
+    <li> {list.item[2]} </li>
+    <li> {list.item[3]} </li>
   </ul>
+);
+
+var listItem = (item) => (
+  <li>{item}</li>
 );
 
 var Chicken = () => (
@@ -14,4 +29,4 @@ var Eggs = () => (
 )
 
 // Use these two new components inside your GroceryList component instead of the hardcoded <li>s
-ReactDOM.render(<GroceryList />, document.getElementById("app"));
+ReactDOM.render(<GroceryList item={['milk', 'eggs', 'cheese', 'butter']}/>, document.getElementById("app"));
